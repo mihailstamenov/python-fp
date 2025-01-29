c8a0584b2c2f6b15185c6f73ac2002c2aa1e74a7
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-30 01:41:31 +0200

    Add Ch 2: Functions As Values, C3: Restore

---
+++ Ch. 2 First Class Functions/C3. Restore/solution/mine.py
@@ -0,0 +1,7 @@
+def restore_documents(originals, backups):
+    return set(
+        map(
+            lambda x: x.upper(), 
+            filter(lambda x: not x.isdigit(),originals+backups)
+        )
+    )   
