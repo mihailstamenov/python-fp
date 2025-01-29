2b20631511b19bb756626152b64e00e75230d6a8
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-01-29 12:10:35 +0200

    Add Ch 2: Functions As Values, L7: Reduce

---
+++ Ch. 2 First Class Functions/07. Reduce/main.py
@@ -0,0 +1,18 @@
+import functools
+
+
+def join(doc_so_far, sentence):
+    if doc_so_far[-1] == ".":
+        return doc_so_far + " " + sentence + "."
+    return doc_so_far + ". " + sentence + "."
+
+
+def join_first_sentences(sentences, n):
+    if n == 0:
+        return ""
+    return functools.reduce(join, sentences[:n])
+
+
+# doc = "asd."
+
+# print(doc[-1])
