969dedb04cb6829bcfb5834ed8aed618a8af69c4
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-02-17 17:46:13 +0200

    Add Ch 5: Function Transformations, L4: More Transformations
    Add Ch 5: Function Transformations, L4: More Transformations

---
+++ Ch. 5 Function Transformations/04. More Transformations/main.py
@@ -0,0 +1,12 @@
+def doc_format_checker_and_converter(conversion_function, valid_formats):
+    pass
+
+# Don't edit below this line
+
+
+def capitalize_content(content):
+    return content.upper()
+
+
+def reverse_content(content):
+    return content[::-1]
