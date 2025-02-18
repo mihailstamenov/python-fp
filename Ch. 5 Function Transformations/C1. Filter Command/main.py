84a03e81cb6ebde00819179108e1bd1b0886729d
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-02-18 10:08:06 +0200

    Add Ch 5: Function Transformations, C1: Filter Command

---
+++ Ch. 5 Function Transformations/C1. Filter Command/main.py
@@ -0,0 +1,19 @@
+def get_filter_cmd(filter_one, filter_two):
+    def filter_cmd(content, option):
+        pass
+    return filter_cmd
+
+
+# don't touch below this line
+
+
+def replace_bad(text):
+    return text.replace("bad", "good")
+
+
+def replace_ellipsis(text):
+    return text.replace("..", "...")
+
+
+def fix_ellipsis(text):
+    return text.replace("....", "...")
