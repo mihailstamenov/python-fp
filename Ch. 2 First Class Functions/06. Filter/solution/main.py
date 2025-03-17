9a451323fea3426c4d81869188c60533b53ebac7
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-03-17 19:41:01 +0200

    Add alt solution Add Ch 2: Functions As Values, L6: Filter

--- Ch. 2 First Class Functions/06. Filter/solution/main.py
+++ Ch. 2 First Class Functions/06. Filter/solution/main.py
@@ -2,3 +2,10 @@ def remove_invalid_lines(document):
     return "\n".join(
         filter(lambda line: not line.startswith("-"), document.split("\n"))
     )
+
+
+# def remove_invalid_lines(document):
+#     split = document.split("\n")
+#     filtered = list(filter(lambda x: x.startswith("*"), split))
+#     res = "\n".join(filtered)
+#     return f"\n{res}\n"
