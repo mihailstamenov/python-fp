e8ef3aaf9d74f296a2bad39a5c63c379aa0ac3ee
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-03-17 21:59:35 +0200

    Add Ch 8: Decorators, L3: Decorators

---
+++ Ch. 8 Decorators/03. Decorators/decorators.py
@@ -0,0 +1,16 @@
+def markdown_to_text_decorator(func):
+    def wrapper(*args, **kwargs):
+        pass
+
+    return wrapper
+
+
+# don't touch below this line
+
+
+def convert_md_to_txt(doc):
+    lines = doc.split("\n")
+    for i in range(len(lines)):
+        line = lines[i]
+        lines[i] = line.lstrip("# ")
+    return "\n".join(lines)
