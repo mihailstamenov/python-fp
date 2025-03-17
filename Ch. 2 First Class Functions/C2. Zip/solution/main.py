b07308b9ba5c58d9fc483bf1ec7d3c4454168316
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-03-17 19:41:58 +0200

    Add alt solution Ch 2: Functions As Values, C2: Zip

--- Ch. 2 First Class Functions/C2. Zip/solution/main.py
+++ Ch. 2 First Class Functions/C2. Zip/solution/main.py
@@ -2,3 +2,8 @@ def pair_document_with_format(doc_names, doc_formats):
     res = list(zip(doc_names, doc_formats))
     final = filter(lambda x: x[1] in valid_formats, res)
     return list(final)
+
+
+# def pair_document_with_format(doc_names, doc_formats):
+#     zipped = list(zip(doc_names, doc_formats))
+#     return list(filter(lambda x: x[1] in valid_formats, zipped))
