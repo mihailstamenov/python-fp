ddc947b8b24dc008e236af0995fc475b2eac78f4
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-03-17 19:40:25 +0200

    Reset starter template Add Ch 2: Functions As Values, L6: Filter

--- Ch. 2 First Class Functions/06. Filter/main.py
+++ Ch. 2 First Class Functions/06. Filter/main.py
@@ -1,5 +1,2 @@
 def remove_invalid_lines(document):
-    split = document.split("\n")
-    filtered = list(filter(lambda x: x.startswith("*"), split))
-    res = "\n".join(filtered)
-    return f"\n{res}\n"
+    pass
