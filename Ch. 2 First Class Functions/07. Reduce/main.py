dcd7e6fa4bdc671f792b8d21249de4b0f771f804
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-03-17 19:41:31 +0200

    Reset starter template Ch 2: Functions As Values, L7: Reduce

--- Ch. 2 First Class Functions/07. Reduce/main.py
+++ Ch. 2 First Class Functions/07. Reduce/main.py
@@ -2,17 +2,8 @@ import functools
 
 
 def join(doc_so_far, sentence):
-    if doc_so_far[-1] == ".":
-        return doc_so_far + " " + sentence + "."
-    return doc_so_far + ". " + sentence + "."
+    pass
 
 
 def join_first_sentences(sentences, n):
-    if n == 0:
-        return ""
-    return functools.reduce(join, sentences[:n])
-
-
-# doc = "asd."
-
-# print(doc[-1])
+    pass
