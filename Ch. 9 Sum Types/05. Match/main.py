60dcf1cabfec95c357b4c470af8501cd27d9f578
Mihail Stamenov <michaelstamenov911@gmail.com>
2025-03-18 10:05:36 +0200

    Add Ch 9: Sum Types, L5: Match

---
+++ Ch. 9 Sum Types/05. Match/main.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class DocFormat(Enum):
+    PDF = 1
+    TXT = 2
+    MD = 3
+    HTML = 4
+
+
+# don't touch above this line
+
+
+def convert_format(content, from_format, to_format):
+    pass
